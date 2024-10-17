a089f3cc9436b7b242530efbfae4ded212e5df30
Dmytro <dmitriy.rebrikov2006@gmail.com>
2024-10-17 14:18:44 +0300

    Создания superuser и заполнение таблиц в бд Изменения, которые необходимо совершить: -db.sqlite3 -products/__pycache__/admin.cpython-312.pyc -products/__pycache__/models.cpython-312.pyc -products/admin.py -products/models.py

--- products/models.py
+++ products/models.py
@@ -3,7 +3,9 @@ from django.db import models
 class ProductCategory(models.Model):
     name = models.CharField(max_length=64, unique=True)
     description =  models.TextField(blank= True)
-    
+    def __str__(self):
+        return f"{self. name}"
+
 
 class Product(models.Model):
     name = models.CharField(max_length=256)
