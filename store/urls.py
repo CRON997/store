86da66665e549d356601ccf4565ff2278ac6ab00
Dmytro <dmitriy.rebrikov2006@gmail.com>
2024-10-19 13:19:49 +0300

    сделал пространство имён url

--- store/urls.py
+++ store/urls.py
@@ -1,13 +1,12 @@
-
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path,include
 from products.views import *
 from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index,name='index'),
-    path('products/', products,name='products'),
+    path('products/',include("products.urls",namespace='products')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
