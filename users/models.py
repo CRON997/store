18bb53691f68510fd6d2dc8512cf4e42058e1fef
CRON997 <dmitriy.rebrikov2006@gmail.com>
2025-02-13 00:00:17 +0200

    комит до характеристик

--- users/models.py
+++ users/models.py
@@ -9,6 +9,7 @@ from django.utils.timezone import now
 class User(AbstractUser):
     image = models.ImageField(upload_to='users_images', blank=True)
     is_verified_email = models.BooleanField(default=False)
+    email = models.EmailField(unique=True)
 
 
 class EmailVerification(models.Model):
