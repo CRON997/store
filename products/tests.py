314ca763ef93b70c0e006784023fabe706943791
CRON997 <dmitriy.rebrikov2006@gmail.com>
2025-05-03 19:44:28 +0300

    перед написанием восстановления пароля

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
-
-# Create your tests here.
+from django.urls import reverse
+from http import HTTPStatus
+from products.models import Product
+class IndexViewTestCase(TestCase):
+    def test_view(self):
+        path = reverse('index')
+        response = self.client.get(path)
+        
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'ElectroHub')
+        self.assertTemplateUsed(response, 'products/index.html')
+        
+        
+class ProductListViewTestCase(TestCase):
+    fixtures = ['products.json', 'categories.json']  # Load test data from fixtures
+    def test_products_list_view(self):
+        path = reverse('products:index')
+        response = self.client.get(path)
+        
+        products = Product.objects.all()
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'ElectroHub - Catalog')
+        self.assertTemplateUsed(response, 'products/products.html')
+        self.assertEqual(response.context_data['object_list'].count(), products.count())
+        
